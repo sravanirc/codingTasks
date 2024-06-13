1e3b691bc5562acbacb5e7848f0fec9877a53510
sravanirc <sravani.rc@gmail.com>
2024-06-13 18:39:16 +0100

    first commit of Nationalize app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// This app lets user input a name and predicts the names nationality
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Alert from "react-bootstrap/Alert";
+import FindNationality from "./components/FindNationality.js";
 
+import { useState, useEffect, useRef } from "react";
 function App() {
+  const [userName, setuserName] = useState("");
+  //using useRef to auto focus on input field
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Alert variant="success">Predict Nationality Of a Name</Alert>
+      <p>
+        Enter a name below to find which country it belongs to with highest
+        probability.
+      </p>
+      <input
+        ref={ref}
+        type="text"
+        placeholder="Enter name here"
+        value={userName}
+        onChange={(event) => {
+          setuserName(event.target.value);
+        }}></input>
+      <FindNationality userName={userName}></FindNationality>
     </div>
   );
 }
---
+++ src/components/FindNationality.js
@@ -0,0 +1,40 @@
+//This components fethces the country id and probability of a username from App
+// then the country id is used to look up the country name and displayed as a <p>
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+
+function FindNationality(props) {
+  const [result, setResult] = useState("");
+
+  function handleClick() {
+    async function API() {
+      //userName is used to fetch data from natinalize.io
+      let response = await fetch(
+        `https://api.nationalize.io?name=${props.userName}`
+      );
+      let data = await response.json();
+      let country = data.country[0].country_id;
+      //from the response, the country code is used to look up country name
+      let resp = await fetch(`https://restcountries.com/v3.1/alpha/${country}`);
+      let nameData = await resp.json();
+      // this is then used to update result stateVariable
+      setResult(
+        `${props.userName} belongs to ${
+          nameData[0].name.common
+        } with a ${parseInt(data.country[0].probability * 100)}% probability.`
+      );
+    }
+
+    API();
+  }
+
+  return (
+    <>
+      <Button onClick={handleClick}>Find Nationality</Button>
+      <p>{result}</p>
+    </>
+  );
+}
+
+export default FindNationality;
